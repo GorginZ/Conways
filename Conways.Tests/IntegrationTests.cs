75eadc2e0c12dabfaa26a1ff15271a5af9fd162e
GorginZ <georgia.z.leng@gmail.com>
2020-12-04 15:43:50 +1100

    can set cells alive

--- Conways.Tests/IntegrationTests.cs
+++ Conways.Tests/IntegrationTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 namespace Conways.Tests
 {
@@ -11,6 +12,16 @@ namespace Conways.Tests
                        + "     \n"
                        + "     \n"
                        + "     \n";
+      Assert.Equal(expectedGrid, ConsoleRenderer.VisualizeGridInConsole(grid.GridClone()));
+    }
+    [Fact]
+    public void CanSetAnInitialState()
+    {
+      var grid = new Grid<CellState>(3, 4);
+      grid.SetMany(new HashSet<RowColumn> { new RowColumn(0, 0), new RowColumn(0, 1), new RowColumn(0, 2) }, CellState.Alive);
+      var expectedGrid = "XXX \n"
+                       + "    \n"
+                       + "    \n";
       Assert.Equal(expectedGrid, ConsoleRenderer.VisualizeGridInConsole(grid.GridClone()));
     }
   }
--- Conways/RowColumn.cs
+++ Conways/RowColumn.cs
@@ -10,6 +10,13 @@ namespace Conways
       Row = row;
       Column = column;
     }
+    public bool Equals(RowColumn other) => (Row, Column).Equals(other);
 
+    public override bool Equals(object obj) => (Row, Column).Equals(obj);
+
+    public static bool operator ==(RowColumn a, RowColumn b) => a.Equals(b);
+    public static bool operator !=(RowColumn a, RowColumn b) => !a.Equals(b);
+
+    public override int GetHashCode() => (Row, Column).GetHashCode();
   }
 }
