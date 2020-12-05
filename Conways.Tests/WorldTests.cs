ecce7c42acd08a8924b8c7a7c69fb45313621e94
GorginZ <georgia.z.leng@gmail.com>
2020-12-05 17:06:59 +1100

    anylivecell with more than 3 dies

--- Conways.Tests/WorldTests.cs
+++ Conways.Tests/WorldTests.cs
@@ -33,11 +33,15 @@ namespace Conways.Tests
     // acceptable range for live cell 2-3
     // any with 3 lives
 
-    // [Fact]
-    // public void AnyLiveCellWithMoreThanThreeLiveNeighboursDies()
-    // {
-
-    // }
+    [Fact]
+    public void AnyLiveCellWithMoreThanThreeLiveNeighboursDies()
+    {
+      var grid = new World(3, 3);
+      grid.SetMany(new HashSet<RowColumn> { new RowColumn(0, 0), new RowColumn(0, 1), new RowColumn(1, 0), new RowColumn(1, 1), new RowColumn(0,2) }, CellState.Alive);
+      Assert.True(grid.IsLive(new RowColumn(0, 0)));
+      grid.Tick();
+      Assert.False(grid.IsLive(new RowColumn(0, 0)));
+    }
     // [Fact]
     // public void AnyLiveCellWithTwoOrThreeLiveNeighboursLives()
     // {
