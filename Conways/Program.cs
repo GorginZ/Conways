40c1c08bf5bc5b86d7ab01a6e750804dc6b22446
GorginZ <georgia.z.leng@gmail.com>
2020-12-05 20:43:23 +1100

    simulation class run func takes an IRenderer and World and renders the simulation

--- Conways/Program.cs
+++ Conways/Program.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace Conways
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-          var indexes = new HashSet<RowColumn>{(new RowColumn(0,0), new RowColumn(1,2), new RowColumn(2,0), new RowColumn(2,1), new RowColumn(2,2), new RowColumn(3,4), new RowColumn(4,5), new RowColumn(5,3), new RowColumn(5,4), new RowColumn(5,5)};
-          var world = new World(10,10, indexes);
-          var renderer = new ConsoleRenderer();
-          var simulation = new Simulation(renderer, world);
-        }
+      var indexes = new HashSet<RowColumn> { new RowColumn(0, 0), new RowColumn(1, 2), new RowColumn(2, 0), new RowColumn(2, 1), new RowColumn(2, 2), new RowColumn(3, 4), new RowColumn(4, 5), new RowColumn(5, 3), new RowColumn(5, 4), new RowColumn(5, 5) };
+      var world = new World(10, 10, indexes);
+      var renderer = new ConsoleRenderer();
+      Simulation.Run(renderer, world);
     }
+  }
 }
--- Conways/Simulation.cs
+++ Conways/Simulation.cs
@@ -1,32 +1,24 @@
+using System.Threading;
+
 namespace Conways
 {
   public class Simulation
   {
-    private IRender _renderer;
-    private World _world;
-    public Simulation(IRender renderer, World world)
-    {
-      _world = world;
-      _renderer = renderer;
-
-    }
     public static void Run(IRender renderer, World world)
     {
-      Thread render = new Thread(() =>
+      var programLock = new object();
+      Thread simulation = new Thread(() =>
                 {
-                  // while (userInput.Command != CurrentCommand.Stop)
                   while (true)
                   {
                     Thread.Sleep(300);
                     lock (programLock)
                     {
-                      renderer.Render(world.Tick());
+                      renderer.Render(world.GridClone());
                       world.Tick();
-
                     }
                   }
-                }); render.Start();
-
+                }); simulation.Start();
     }
   }
 }
