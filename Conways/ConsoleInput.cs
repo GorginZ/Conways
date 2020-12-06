0fa16a424b8eaea391bd6fce3e2f87fec48d1348
GorginZ <georgia.z.leng@gmail.com>
2020-12-06 22:29:02 +1100

    IRead interface and ConsoleInput class

---
+++ Conways/ConsoleInput.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Conways
+{
+  public class ConsoleInput : IRead
+  {
+    public string ReadInput(string prompt)
+    {
+      Console.WriteLine(prompt);
+      return Console.ReadLine();
+    }
+    public (int, int) GetDimensions()
+    {
+        var rowInput = ReadInput("How many rows?");
+        var colInput = ReadInput("How many columns?");
+        var parsedRow = int.TryParse(rowInput, out int rowValue);
+        var parsedCol = int.TryParse(colInput, out int colValue);
+        if (parsedRow && parsedCol && IsValid(rowValue) && IsValid(colValue))
+        {
+          return (rowValue, colValue);
+        }
+    }
+    public static bool IsValid(int value) => value <= 50;
+  }
+}
