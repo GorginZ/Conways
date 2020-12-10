fc6e124713d9f83e4eee42faf1a9987fede32af5
GorginZ <georgia.z.leng@gmail.com>
2020-12-10 17:05:57 +1100

    tests for parser

--- Conways.Tests/InputParserTests.cs
+++ Conways.Tests/InputParserTests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Xunit;
 namespace Conways.Tests
 {
   public class InputParserTests
@@ -8,17 +10,22 @@ namespace Conways.Tests
       var input = "0,0 3,3 5,5";
       var dimensions = (6, 6);
       var indexes = InputParser.ParseInputToValidIndexes(input, dimensions);
-      Assert.True(indexes.SetEquals(new HashSet<(int, int)>((0, 0), (3, 3), (5, 5))));
+      Assert.True(indexes.SetEquals(new HashSet<(int, int)>{(0, 0), (3, 3), (5, 5)}));
     }
-    [Fact]
-    public void CanHandleTyposAndIgnoresThem()
-    {
+    // [Fact]
+    // public void CanHandleTyposAndIgnoresThem()
+    // {
+    //   var input = "0dfjaklsjf 0,0 3,3 5,5";
+    //   var dimensions = (6, 6);
+    //   var indexes = InputParser.ParseInputToValidIndexes(input, dimensions);
+    //   Assert.True(indexes.SetEquals(new HashSet<(int, int)>((0, 0), (3, 3), (5, 5))));
 
-    }
-    [Fact]
-    public void WontParseDuplicatInputIndexes()
-    {
 
-    }
+    // }
+    // [Fact]
+    // public void WontParseDuplicatInputIndexes()
+    // {
+
+    // }
   }
 }
